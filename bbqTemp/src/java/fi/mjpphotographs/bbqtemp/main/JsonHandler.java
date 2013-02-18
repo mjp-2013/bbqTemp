ef49520a528f6555084654aa6c1fed2a0ce011c1
Mikko <>
2013-02-18 19:05:05 +0200

    Added more call functions

--- bbqTemp/src/java/fi/mjpphotographs/bbqtemp/main/JsonHandler.java
+++ bbqTemp/src/java/fi/mjpphotographs/bbqtemp/main/JsonHandler.java
@@ -31,7 +31,7 @@ import org.apache.commons.configuration.Configuration;
 import org.apache.log4j.Logger;
 
 /**
- * Json support 
+ * Json support
  *
  * @author MjP
  */
@@ -59,65 +59,39 @@ class JsonHandler
 
         if ( null != jsonActionName && jsonActionName.equalsIgnoreCase( "getLastHourData" ) )
         {
-            getLastHourlyData( request, response );
+            getData( request, response, 60 );
         }
-
+        else if ( null != jsonActionName && jsonActionName.equalsIgnoreCase( "getAlltimeData" ) )
+        {
+            getData( request, response, -1 );
+        }
     }
 
     /**
-     * This method is used for averaging temperature data arrays (List<Temperature>). Mainly used for chart drawing purposes. This is thread safe implementation (creates new/clone objects for output list)
-     * @param groupSize How many samples is taken for average.
-     * @param temperatures List of Temperature objects.
-     * @return Returns new array of Temperature objects which has been averaged. The first occurence of logDatetime of current group is used for time.
+     * Returns selected hours of temperature data.
+     *
+     * @param request
+     * @param response
+     * @param minutes how many minutes of data is returned. -1 for all data.
      */
-    private List<Temperature> averageData( int groupSize, List<Temperature> temperatures )
+    private void getData( HttpServletRequest request, HttpServletResponse response, int minutes )
     {
-        List<Temperature> returnArray = new ArrayList();
-        int j = 0;
-        float sum = 0;
- 
-        Date firstDate = null;
-        for ( int i = 0; i < temperatures.size(); i++ )
+        List<Temperature> temperatures = new ArrayList();
+        Date startDate = null;
+        if ( minutes == -1 )
         {
-            if (j == 0){
-               firstDate = temperatures.get( i ).getLogDatatime(); 
-            }
-            if ( j < groupSize )
-            {
-              sum += temperatures.get( i ).getMjTemperature();
-                j++;                
-            }
-
-            if ( j == groupSize )
-            {
-                Temperature tmpTemperature = new Temperature();
-                tmpTemperature.setMjTemperature( (sum / groupSize) );
-                tmpTemperature.setLogDatatime( (Date)firstDate.clone() );
-                returnArray.add( tmpTemperature );
-                j = 0;
-                sum = 0;  
-            }    
+            //TODO think better aproach of this, possibilities for errors.. RPI resets its time epoch or last used time (fake hardware clock), but this should get all data if db is not corrupted or datime is not chaged pre 1970s.
+            startDate = new Date( 0 );  
         }
-        if ( sum > 0 )
+        else
         {
-            Temperature tmpTemperature = new Temperature();
-            tmpTemperature.setMjTemperature( (sum / j) );
-            tmpTemperature.setLogDatatime( firstDate );
-            returnArray.add( tmpTemperature );
+            // get selected amount of minutes
+            startDate = new Date( System.currentTimeMillis() - minutes * 60 * 1000 );
+
         }
 
-        return returnArray;
-    }
-
-    // quick and dirty json... 
-    private void getLastHourlyData( HttpServletRequest request, HttpServletResponse response )
-    {
-        List<Temperature> temperatures = new ArrayList(); 
-        // get -1 hour
-        Date startDate = new Date( System.currentTimeMillis() - 3600 * 1000 );
-        
         try
-        {  
+        {
             temperatures = tempDAO.getTemperatures( startDate, new Date() );
         }
         catch ( DAOException ex )
@@ -126,32 +100,10 @@ class JsonHandler
             //TODO exception
         }
 
-        //Making datagroup much smaller (calculating averages of every minute log events)
+        //Making datagroup much smaller (calculating averages of every minute log events or so)
         temperatures = averageData( 30, temperatures );
-        
-        StringBuilder sb = new StringBuilder();
 
-        sb.append( "[{\"data\":[" );
-
-        for ( int i = 0; i < temperatures.size(); i++ )
-        {
-            SimpleDateFormat sdf = new SimpleDateFormat( "yyyy-MM-dd h:mma" );
-
-            String formatedTimestamp = sdf.format( temperatures.get( i ).getLogDatatime() );
-
-            sb.append( "[\"" ).append( formatedTimestamp ).append( "\"");
-
-            sb.append( "," ).append( temperatures.get( i ).getMjTemperature() ).append( "]");
-
-            if ( i + 1 < temperatures.size() )
-            {
-                sb.append( "," );
-            }
-
-        }
-        sb.append( "]}]" );
-
-        String json = sb.toString();
+        String json = constructJsonFromTemperatures( temperatures );
 
         response.setContentType( "application/json" );
         response.setCharacterEncoding( "UTF-8" );
@@ -165,5 +117,82 @@ class JsonHandler
 
             logger.debug( "JSON response data:" + json );
         }
+    }
+
+    public String constructJsonFromTemperatures( List<Temperature> temperatures )
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append( "[{\"data\":[" );
+
+        for ( int i = 0; i < temperatures.size(); i++ )
+        {
+            SimpleDateFormat sdf = new SimpleDateFormat( "yyyy-MM-dd h:mma" );
+
+            String formatedTimestamp = sdf.format( temperatures.get( i ).getLogDatatime() );
+
+            sb.append( "[\"" ).append( formatedTimestamp ).append( "\"" );
+
+            sb.append( "," ).append( temperatures.get( i ).getMjTemperature() ).append( "]" );
+
+            if ( i + 1 < temperatures.size() )
+            {
+                sb.append( "," );
+            }
+
+        }
+        sb.append( "]}]" );
+
+        return sb.toString();
+    }
+
+    /**
+     * This method is used for averaging temperature data arrays
+     * (List<Temperature>). Mainly used for chart drawing purposes. This is
+     * thread safe implementation (creates new/clone objects for output list)
+     *
+     * @param groupSize How many samples is taken for average.
+     * @param temperatures List of Temperature objects.
+     * @return Returns new array of Temperature objects which has been averaged.
+     * The first occurence of logDatetime of current group is used for time.
+     */
+    private List<Temperature> averageData( int groupSize, List<Temperature> temperatures )
+    {
+        List<Temperature> returnArray = new ArrayList();
+        int j = 0;
+        float sum = 0;
+
+        Date firstDate = null;
+        for ( int i = 0; i < temperatures.size(); i++ )
+        {
+            if ( j == 0 )
+            {
+                firstDate = temperatures.get( i ).getLogDatatime();
+            }
+            if ( j < groupSize )
+            {
+                sum += temperatures.get( i ).getMjTemperature();
+                j++;
+            }
+
+            if ( j == groupSize )
+            {
+                Temperature tmpTemperature = new Temperature();
+                tmpTemperature.setMjTemperature( (sum / groupSize) );
+                tmpTemperature.setLogDatatime( ( Date ) firstDate.clone() );
+                returnArray.add( tmpTemperature );
+                j = 0;
+                sum = 0;
+            }
+        }
+        if ( sum > 0 )
+        {
+            Temperature tmpTemperature = new Temperature();
+            tmpTemperature.setMjTemperature( (sum / j) );
+            tmpTemperature.setLogDatatime( firstDate );
+            returnArray.add( tmpTemperature );
+        }
+
+        return returnArray;
     }
 }
